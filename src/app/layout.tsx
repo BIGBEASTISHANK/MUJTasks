6c1959aebf81ae5ba1d779898fae3b10be1955fa
BIGBEASTISHANK <ishank@bigbeastishank.com>
2025-04-22 00:41:00 +0530

    Added counter.dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
       className="!scroll-smooth bg-[#1A1E23] text-white"
       suppressHydrationWarning={true}
     >
+      <head>
+        <script
+          src="https://cdn.counter.dev/script.js"
+          data-id="97b223ea-ee69-4ce7-8e2c-dd4875f6565a"
+          data-utcoffset="6"
+        ></script>
+      </head>
       <body className={`${geistMono.className}`}>
         <Navbar />
         {children}
