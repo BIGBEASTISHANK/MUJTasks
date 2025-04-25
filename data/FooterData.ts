fef46c812ef2c66b0181a51e19665aebf9df182c
BIGBEASTISHANK <ishank@bigbeastishank.com>
2025-04-25 23:17:27 +0530

    Added Whatsapp group link to website footer

--- data/FooterData.ts
+++ data/FooterData.ts
@@ -1,10 +1,14 @@
 import { IconType } from "react-icons";
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 export const FooterSocialData: { icon: IconType; href: string }[] = [
   {
     icon: FaGithub,
-    href: "https://github.com/BIGBEASTISHANK",
+    href: "https://github.com/BIGBEASTISHANK/MUJTasks",
+  },
+  {
+    icon: FaWhatsapp,
+    href: "https://chat.whatsapp.com/HsAgoqke1PfLakSE0yMdqj",
   },
   {
     icon: FaInstagram,
